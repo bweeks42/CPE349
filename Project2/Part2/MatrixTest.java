ee1966f986bbfc9209c7ffc29053c14963b6bde8
Audrey Chan <audreyc@pcp140550pcs.wireless.calpoly.edu>
2017-01-30 10:01:43 -0800

    add strassen

--- Project2/Part2/MatrixTest.java
+++ Project2/Part2/MatrixTest.java
@@ -1,12 +1,22 @@
 import java.util.Random;
 import java.util.Arrays;
-class MatrixTest {
+
+public class MatrixTest {
+	
+	public static final String ANSI_GREEN = "\u001B[32m";
+   	public static final String ANSI_RED = "\u001B[31m";
+   	public static final String ANSI_RESET = "\u001B[0m";
 
 	public static void main(String args[]) {
-		
-		Random rand = new Random();
+		int maxSize = 800;
+		DAC_Tests(maxSize);
+		Strassen_Tests(maxSize);
+		System.out.println("End of output");
+	}
 
-		for (int i = 1; i < 10; i++) {
+	private static void DAC_Tests(int maxSize) {
+		System.out.println("DAC PRODUCT TESTS:");
+		for (int i = 1; i < maxSize; i*= 2) {
 			int size = i;
 			int [][] testA = generateSquareMatrix(size);	
 			int [][] testB = generateSquareMatrix(size);
@@ -18,17 +28,53 @@ class MatrixTest {
 				isCorrect &= Arrays.equals(result[j], solution[j]);
 			}
 
+			/*System.out.println("The result is:");
+			for (int k = 0; k < size; k++) {
+				for (int l = 0; l < size; l++) {
+					System.out.print(result[k][l] + " ");
+				}
+				System.out.println("");
+			}
+
+			System.out.println("CORRECT solution is:");
+			for (int m = 0; m < size; m++) {
+				for (int n = 0; n < size; n++) {
+					System.out.print(solution[m][n] + " ");
+				}
+				System.out.println("");
+			}*/
+
 			if (isCorrect) {
-				System.out.println("Passed.");
+				System.out.println(ANSI_GREEN + "Passed" + ANSI_RESET);
 			}
 			else {
-				System.out.println("Failed.");
+				System.out.println(ANSI_RED + "Failed" + ANSI_RESET);
 			}
-		}
-
+		}	
 	}
 
+	private static void Strassen_Tests(int maxSize) {
+		System.out.println("STRASSEN PRODUCT TESTS:");
+		for (int i = 1; i < maxSize; i*= 2) {
+			int size = i;
+			int [][] testA = generateSquareMatrix(size);	
+			int [][] testB = generateSquareMatrix(size);
+			int [][] result = MatrixProduct.matrixProduct_Strassen(testA, testB);
+			int [][] solution = MatrixWork.matrixProduct(testA, testB);
 
+			boolean isCorrect = true;
+			for (int j = 0; j < size; j++) {
+				isCorrect &= Arrays.equals(result[j], solution[j]);
+			}
+
+			if (isCorrect) {
+				System.out.println(ANSI_GREEN + "Passed" + ANSI_RESET);
+			}
+			else {
+				System.out.println(ANSI_RED + "Failed" + ANSI_RESET);
+			}
+		}	
+	}
 
 	private static int[][] generateSquareMatrix(int size) {
 
